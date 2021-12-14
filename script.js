c55a1a6d992c700b367a0d1d4327ca4a7c2fd2b3
Nick Garza <thenickgarza14@gmail.com>
2021-12-14 17:10:27 -0600

    have the data showing on the page

--- script.js
+++ script.js
@@ -1,7 +1,10 @@
  // global variables
  var cityInputEl = document.querySelector('#citySearch');
  var cityFormEl = document.querySelector('#city-form');
- var button = document.querySelector('btn');
+ var button = document.querySelector('.btn');
+ var nameEl = document.querySelector('.name');
+ var text = document.querySelector('.text');
+ var temp = document.querySelector('.temp');
  // tells computer to run HTML & CSS 1st.
  $(document).ready(function () {
     //displays the current day & time using moment js
@@ -32,11 +35,24 @@
  var getCityWeather = function(city) {
      // format the OpenWeatherApi url
      var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=5e5aeafab0a91097731db1eeb71889ff';
-
      // make a request to the url
      fetch(apiUrl)
+     // gets the response and formats it to json
      .then(response => response.json())
-     .then(data => console.log(data.main))
+     // get the json response the console logs the data. main of the object
+    //  .then(data => console.log(data))
+
+
+    .then(data => {
+        var nameValue = data['name'];
+        var tempature = data['main']
+        var descValue = data['weather'][0]['description']
+
+        nameEl.innerHTML = nameValue;
+        temp.innerHTML = tempature;
+        descValue.innerHTML = descValue;
+        console.log(tempature)
+    })
  };
 
  cityFormEl.addEventListener("submit", citySubmitHandler);

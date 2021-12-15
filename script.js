f22be5d3ada396813d9379f948634b1cc6205696
Nick Garza <thenickgarza14@gmail.com>
2021-12-15 10:34:26 -0600

    Have the 1st part of the current weather showing up, still need to get the 5 day

--- script.js
+++ script.js
@@ -2,19 +2,17 @@
  var cityInputEl = document.querySelector('#citySearch');
  var cityFormEl = document.querySelector('#city-form');
  var button = document.querySelector('.btn');
- var nameEl = document.querySelector('.name');
- var text = document.querySelector('.text');
- var temp = document.querySelector('.temp');
+ var nameEl = document.querySelector('#user-city');
+ var windEl = document.querySelector('#wind');
+ var tempEl = document.querySelector('#temp');
+ var humidityEl = document.querySelector('#humidity');
+ var uvIndexEl = document.querySelector('#uvIndex');
  // tells computer to run HTML & CSS 1st.
  $(document).ready(function () {
     //displays the current day & time using moment js
     $("#currentTime").text(moment().format("MMMM Do YYYY, h:mm:ss a"));
  });
   
-//  fetch("https://api.openweathermap.org/data/2.5/weather?q=austin&appid=5e5aeafab0a91097731db1eeb71889ff")
-//  .then(response => response.json())
-//  .then(data => console.log(data))
-
  var citySubmitHandler = function(event) {
      // prevent page from refreshing
      event.preventDefault();
@@ -34,24 +32,24 @@
 
  var getCityWeather = function(city) {
      // format the OpenWeatherApi url
-     var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=5e5aeafab0a91097731db1eeb71889ff';
+     var apiUrl = 'https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=5e5aeafab0a91097731db1eeb71889ff&units=standard';
      // make a request to the url
      fetch(apiUrl)
      // gets the response and formats it to json
      .then(response => response.json())
      // get the json response the console logs the data. main of the object
     //  .then(data => console.log(data))
-
-
     .then(data => {
-        var nameValue = data['name'];
-        var tempature = data['main']
-        var descValue = data['weather'][0]['description']
-
-        nameEl.innerHTML = nameValue;
-        temp.innerHTML = tempature;
-        descValue.innerHTML = descValue;
-        console.log(tempature)
+        var cityName = data.name
+        var wind = data.wind.speed
+        var humidity = data.main.humidity
+        console.log(data);
+        // puts the data into the HTML 
+        nameEl.innerHTML = " City: " + cityName 
+        windEl.innerHTML = " Wind: " + wind + " MPH ";
+        humidityEl.innerHTML = " Humidity: " + humidity + "%";
+        
+        
     })
  };
 

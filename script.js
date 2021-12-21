bb7e96f5b4e67b07cf39081de1551e8bd48fcf89
Nick Garza <thenickgarza14@gmail.com>
2021-12-21 00:31:16 -0600

    used math.round to round the wind speed and temppature

--- script.js
+++ script.js
@@ -63,8 +63,8 @@
      // get the json response the console logs the data. main of the object
     .then(data => {
         var cityName = data.name
-        var temp = data.main.temp
-        var wind = data.wind.speed
+        var temp = Math.round(data.main.temp)
+        var wind = Math.round(data.wind.speed)
         var humidity = data.main.humidity
         console.log(data);
         // puts the data into the HTML 
@@ -83,32 +83,32 @@
             .then(data => {
                 console.log(data)
                 // Monday Forecast
-                mTemp.innerHTML = " Temp: " + data.daily[0].temp.day + " °F "
-                mWind.innerHTML = " Wind: " + data.daily[0].wind_speed + " MPH "
+                mTemp.innerHTML = " Temp: " + Math.round(data.daily[0].temp.day) + " °F "
+                mWind.innerHTML = " Wind: " + Math.round(data.daily[0].wind_speed) + " MPH "
                 mHumid.innerHTML = " Humidity: " + data.daily[0].humidity + " % "
                 // Tuesday Forecast
-                tTemp.innerHTML = " Temp: " + data.daily[1].temp.day + " °F "
-                tWind.innerHTML = " Wind: " + data.daily[1].wind_speed + " MPH "
+                tTemp.innerHTML = " Temp: " + Math.round(data.daily[1].temp.day) + " °F "
+                tWind.innerHTML = " Wind: " + Math.round(data.daily[1].wind_speed) + " MPH "
                 tHumid.innerHTML = " Humidity: " + data.daily[1].humidity + " % "
                 // Wednesday Forecast
-                wTemp.innerHTML = " Temp: " + data.daily[2].temp.day + " °F "
-                wWind.innerHTML = " Wind: " + data.daily[2].wind_speed + " MPH "
+                wTemp.innerHTML = " Temp: " + Math.round(data.daily[2].temp.day) + " °F "
+                wWind.innerHTML = " Wind: " + Math.round(data.daily[2].wind_speed) + " MPH "
                 wHumid.innerHTML = " Humidity: " + data.daily[2].humidity + " % "
                 // Thursday Forecast
-                thTemp.innerHTML = " Temp: " + data.daily[3].temp.day + " °F "
-                thWind.innerHTML = " Wind: " + data.daily[3].wind_speed + " MPH "
+                thTemp.innerHTML = " Temp: " + Math.round(data.daily[3].temp.day) + " °F "
+                thWind.innerHTML = " Wind: " + Math.round(data.daily[3].wind_speed) + " MPH "
                 thHumid.innerHTML = " Humidity: " + data.daily[3].humidity + " % "
                 // Friday Forecast
-                fTemp.innerHTML = " Temp: " + data.daily[4].temp.day + " °F "
-                fWind.innerHTML = " Wind: " + data.daily[4].wind_speed + " MPH "
+                fTemp.innerHTML = " Temp: " + Math.round(data.daily[4].temp.day) + " °F "
+                fWind.innerHTML = " Wind: " + Math.round(data.daily[4].wind_speed) + " MPH "
                 fHumdid.innerHTML = " Humidity: " + data.daily[4].humidity + " % "
                 // Saturday Forecast
-                sTemp.innerHTML = " Temp: " + data.daily[5].temp.day + " °F "
-                sWind.innerHTML = " Wind: " + data.daily[5].wind_speed + " MPH "
+                sTemp.innerHTML = " Temp: " + Math.round(data.daily[5].temp.day) + " °F "
+                sWind.innerHTML = " Wind: " + Math.round(data.daily[5].wind_speed) + " MPH "
                 sHumid.innerHTML = " Humidity: " + data.daily[5].humidity + " % "
                 // Sunday Forecast
-                suTemp.innerHTML = " Temp: " + data.daily[6].temp.day + " °F "
-                suWind.innerHTML = " Wind: " + data.daily[6].wind_speed + " MPH "
+                suTemp.innerHTML = " Temp: " + Math.round(data.daily[6].temp.day) + " °F "
+                suWind.innerHTML = " Wind: " + Math.round(data.daily[6].wind_speed) + " MPH "
                 suHumid.innerHTML = " Humidity: " + data.daily[6]. humidity + " % "
 });
         
